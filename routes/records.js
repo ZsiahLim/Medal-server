5cb8cef0371baf9a92b7fe49809bb2ad9078f413
leon666 <qinleiheng@gmail.com>
2024-01-14 16:20:04 +0800

    feat: add get other user records function

--- routes/records.js
+++ routes/records.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../verifyToken.js'
-import { deleteRecord, getLatestRecord, getRecords, uploadRecord } from '../controllers/record.js'
+import { deleteRecord, getLatestRecord, getRecords, getRecordsByUserId, uploadRecord } from '../controllers/record.js'
 
 const router = express.Router()
 // router.post('/', createBlog)
@@ -9,6 +9,7 @@ router.post('/', verifyToken, uploadRecord)
 router.delete('/:id', verifyToken, deleteRecord)
 
 router.get('/', verifyToken, getRecords)
+router.get('/:id', verifyToken, getRecordsByUserId)
 
 router.get('/latest', verifyToken, getLatestRecord)
 
